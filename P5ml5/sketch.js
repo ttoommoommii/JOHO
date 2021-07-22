2ea015b04984c55d68d5cff677636873e430f838
ttoommoommii <69040360+ttoommoommii@users.noreply.github.com>
2021-07-22 13:23:19 +0900

    2021

--- P5ml5/sketch.js
+++ P5ml5/sketch.js
@@ -1,72 +1,26 @@
 let classifier;
 let video;
 let label = "";
+let resultsP;//
 
-let constraints=0;    //カメラ変更
 let img;//画像データ
 
 
-function setup() {
 
-    if (!constraints) {
-        //constraints = { video: true, audio: false };
-        constraints = {video: {facingMode: 'environment'}, audio: false }; //'user'          
-    }
+
+function setup() {
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
-
-    classifier = ml5.imageClassifier("MobileNet", video, modelLoaded);
-
-    //ボタンの追加
-    CamChangeButton1=createButton("CamChange[背面]");
-    CamChangeButton1.mousePressed(CamChangeBak);
-    CamChangeButton1.size(200,30);
-    CamChangeButton1.position(width/2 - 210, height);
+    // Initialize the Image Classifier method with MobileNet and the video as the second argument
+    classifier = ml5.imageClassifier('MobileNet', video, modelReady);
     
-    CamChangeButton2=createButton("CamChange[顔面]");
-    CamChangeButton2.mousePressed(CamChangeFront);
-    CamChangeButton2.size(200,30);
-    CamChangeButton2.position(width/2 + 10, height);
-    //img = loadImage('img.jpg'); // Load the image
-    
+    resultsP = createP('Loading model and video...');
 }
+
 function preload() {
   img = loadImage('img/img2.png');
 }
-function CamChangeBak(){
-    //if (!constraints) {
-        //constraints = { video: true, audio: true };
-        constraints = {
-           video: {
-            // スマホのバックカメラを使用
-            facingMode: 'user'//'environment' //'user'BAK
-        }, audio: false };          
-    //}
-    //createCanvas(640, 480);
-    
-    video = createCapture(VIDEO);
-    video.hide();
-
-    classifier = ml5.imageClassifier("MobileNet", video, modelLoaded);
-}
-function CamChangeFront(){
-     //if (!constraints) {
-        //constraints = { video: true, audio: true };
-        constraints = {
-           video: {
-            // スマホのバックカメラを使用
-            facingMode: 'environment'//'environment' //'user'BAK
-        }, audio: false };          
-    //}
-    //createCanvas(640, 480);
-   
-    video = createCapture(VIDEO);
-    video.hide();
-
-    classifier = ml5.imageClassifier("MobileNet", video, modelLoaded);
-}
-
 
 function draw() {
     image(video, 0, 0);
@@ -75,22 +29,29 @@ function draw() {
     strokeWeight(3);
     stroke(20, 181, 255);
     text("カメラ映像から推測すると！！",80,height - 40)
-    textSize(25);
+    textSize(20);
     text(label, 110, height - 10);
     image(img, 0, height - 69, 70, 70);
 }
 
-function modelLoaded() {
-    console.log("Model Loaded!");
-    classifier.predict(gotResults);
-}
-
-function gotResults(err, results) {
+function modelReady() {
+    console.log('Model Ready');
+    classifyVideo();
+  }
+  
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    classifier.classify(gotResult);
+  }
+  
+  // When we get a result
+  function gotResult(err, results) {
+    // The results are in an array ordered by confidence.
     if (err) {
         console.error(err);
     } else {
-        console.log(results);
-        label = results[0].className;
-        classifier.predict(gotResults);
+    resultsP.html('信頼度：' + nf(results[0].confidence, 0, 2));
+    label=results[0].label;
+    classifyVideo();
     }
-}
+  }
